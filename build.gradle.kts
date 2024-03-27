94ccdb83fd319b0f753e48e0dc5b746b9f62279c
Maciej Procyk <macieekprocyk@gmail.com>
2024-03-27 22:04:52 +0100

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
@@ -8,5 +10,40 @@ plugins {
     alias(libs.plugins.buildKonfig) apply false
     alias(libs.plugins.graalVM) apply false
 
+    alias(libs.plugins.detekt)
     alias(libs.plugins.dotenvGradle)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+
+    config.setFrom("config/detekt/detekt.yml")
+
+    source.setFrom(
+        "composeApp/src/androidMain/kotlin",
+        "composeApp/src/commonMain/kotlin",
+        "composeApp/src/desktopMain/kotlin",
+        "composeApp/src/iosMain/kotlin",
+        "composeApp/src/wasmJsMain/kotlin",
+
+        "server/src/main/kotlin",
+
+        "shared/src/commonMain/kotlin",
+    )
+
+    autoCorrect = true
+}
+
+tasks.withType(Detekt::class) {
+    reports {
+        html.required.set(true)
+        md.required.set(false)
+        xml.required.set(false)
+        sarif.required.set(false)
+        txt.required.set(false)
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
 }
