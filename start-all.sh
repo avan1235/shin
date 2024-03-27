10eea53242cfbe327f9ee92bea30d5aea052b548
Maciej Procyk <macieekprocyk@gmail.com>
2024-03-27 20:27:11 +0100

    cleanup unused scripts

--- start-all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose --file docker-compose.yml --env-file .env build
-docker compose --file docker-compose.yml --env-file .env up
--- start-db.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose --file docker-compose.yml --env-file .env build shin-dev-postgres
-docker compose --file docker-compose.yml --env-file .env up shin-dev-postgres
